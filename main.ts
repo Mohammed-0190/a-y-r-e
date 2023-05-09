72f520701589a9866a7ca286e044cdef19cdf6f4
Mohammed-0190 <131755029+Mohammed-0190@users.noreply.github.com>
2023-05-09 10:23:43 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.showLeds(`
+    # # # # #
+    . # # # #
+    . . # . .
+    . # # # .
+    # # # # #
+    `)
+basic.showIcon(IconNames.Heart)
+basic.setLedColor(0xff0000)
+music.playMelody("- E C C E F B A ", 120)
+if (true) {
+	
+}
